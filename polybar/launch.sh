fe597ab8b50b0310137fcb872f1043ad7c2a8355
Aristides Gonzalez <ariguille.gp@gmail.com>
2020-11-18 18:32:40 -0500

    added polybar to multiple monitors

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -6,7 +6,15 @@ killall -q polybar
 # polybar-msg cmd quit
 
 # Launch bar1 and bar2
-echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar example >>/tmp/polybar1.log 2>&1 & disown
+#echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
+#polybar example >>/tmp/polybar1.log 2>&1 & disown
+
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload example &
+  done
+else
+  polybar --reload example &
+fi
 
 echo "Bars launched..."

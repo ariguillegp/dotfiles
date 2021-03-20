2e4440105e3878afd6919d4a67eb8e045bfd9003
Aristides Gonzalez <ariguille.gp@gmail.com>
2021-03-20 17:50:12 -0400

    Added vagrant, virtualbox and ansible

---
+++ install-ansible.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+## sshpass is a requirement to authenticate with regular passwords
+## vim-ansible improves vim support
+sudo pacman -S --noconfirm --needed ansible sshpass vim-ansible
+
+## Validate installation
+ansible --version
+
+## Successful exit
+exit 0
---
+++ install-vm-tools.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+## Install virtualbox, kernel modules and guest additions
+sudo pacman -S --noconfirm --needed virtualbox virtualbox-host-modules-arch virtualbox-guest-iso
+
+## Install extension package
+yay -S virtualbox-ext-oracle
+
+## Install vagrant
+sudo pacman -S --noconfirm --needed vagrant
+vagrant --version
+
+## Successful exit
+exit 0

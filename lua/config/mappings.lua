3d8f87129c045e45491b3325925dbaf84bea9411
Aristides Gonzalez <ariguille.gp@gmail.com>
2021-04-11 23:18:45 -0400

    some mappings in lua

--- lua/config/mappings.lua
+++ lua/config/mappings.lua
@@ -1,1 +1,7 @@
 local utils = require('config.utils')
+
+utils.map('n', '<Leader>h', '<cmd>wincmd h<CR>')
+utils.map('n', '<Leader>j', '<cmd>wincmd j<CR>')
+utils.map('n', '<Leader>k', '<cmd>wincmd k<CR>')
+utils.map('n', '<Leader>l', '<cmd>wincmd l<CR>')
+utils.map('n', '<Leader>pv', '<cmd>wincmd v<bar> :Ex <bar> :vertical resize 30<CR>')
--- plugin/navigation.vim
+++
@@ -1,6 +0,0 @@
-nnoremap <leader>h :wincmd h<CR>
-nnoremap <leader>j :wincmd j<CR>
-nnoremap <leader>k :wincmd k<CR>
-nnoremap <leader>l :wincmd l<CR>
-nnoremap <leader>u :UndotreeShow<CR>
-nnoremap <leader>pv :wincmd v<bar> :Ex <bar> :vertical resize 30<CR>

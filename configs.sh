111ea0eb9f86c23e3c3f15323919175e935091b4
Aristides Gonzalez <ariguille.gp@gmail.com>
2021-10-15 18:55:02 -0400

    improved auto stow

--- configs.sh
+++ configs.sh
@@ -2,6 +2,6 @@
 
 # TODO: Write something to take care
 # of the initial files not owned by stow
-find . -maxdepth 1 -name '*.conf' -exec  stow {} \;
+find * -maxdepth 0 -name '*.conf' -exec  stow {} \;
 
 exit 0

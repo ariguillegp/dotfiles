0569bd5e42a6101586dd2f65c4d6d956221d17ef
Aristides Gonzalez <ariguille.gp@gmail.com>
2021-10-20 02:13:51 -0400

    more useful remaps -- breaking undos

--- configs/nvim/.config/nvim/lua/config/mappings.lua
+++ configs/nvim/.config/nvim/lua/config/mappings.lua
@@ -1,7 +1,21 @@
 local utils = require('config.utils')
 
+--- behave like C and D
 utils.map('n', 'Y', 'y$')
 
+--- keep it centered
+utils.map('n', 'n', 'nzzzv')
+utils.map('n', 'N', 'Nzzzv')
+utils.map('n', 'J', 'mzJ`z')
+
+--- undo breakpoints
+utils.map('i', ',', ',<c-g>u')
+utils.map('i', '.', '.<c-g>u')
+utils.map('i', '[', '[<c-g>u')
+utils.map('i', '!', '!<c-g>u')
+utils.map('i', '?', '?<c-g>u')
+
+--- window navigation with vim keys
 utils.map('n', '<Leader>h', '<cmd>wincmd h<CR>')
 utils.map('n', '<Leader>j', '<cmd>wincmd j<CR>')
 utils.map('n', '<Leader>k', '<cmd>wincmd k<CR>')

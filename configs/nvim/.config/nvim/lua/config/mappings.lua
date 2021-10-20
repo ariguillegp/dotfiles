55ece5915f4fa86e4f1fb3537c5a96b492f71911
Aristides Gonzalez <ariguille.gp@gmail.com>
2021-10-20 01:02:07 -0400

    Y needs to behave like C and D

--- configs/nvim/.config/nvim/lua/config/mappings.lua
+++ configs/nvim/.config/nvim/lua/config/mappings.lua
@@ -1,5 +1,7 @@
 local utils = require('config.utils')
 
+utils.map('n', 'Y', 'y$')
+
 utils.map('n', '<Leader>h', '<cmd>wincmd h<CR>')
 utils.map('n', '<Leader>j', '<cmd>wincmd j<CR>')
 utils.map('n', '<Leader>k', '<cmd>wincmd k<CR>')

f4fc5cf97a273d346a1c7d33bec68282c48877cb
Aristides Gonzalez <ariguille.gp@gmail.com>
2021-11-15 14:59:05 -0500

    Moving things around for lua migration and improving comments

--- configs/nvim/.config/nvim/init.lua
+++ configs/nvim/.config/nvim/init.lua
@@ -1,21 +1,22 @@
 -- Bear in mind that the Lua API is still fairly beta,
 -- and many Vim things don’t have direct interfaces.
 
--- Map leader to space
+-- Map leader key to space
 vim.g.mapleader = ' '
 
 -- some local variables for better readability
 local fn = vim.fn
 local execute = vim.api.nvim_command
+local cmd = vim.cmd
 
--- Auto install packer.nvim if not exists
+-- Auto install packer.nvim if it doesn't exist
 local install_path = fn.stdpath('data')..'/site/pack/packer/opt/packer.nvim'
 if fn.empty(fn.glob(install_path)) > 0 then
   execute('!git clone https://github.com/wbthomason/packer.nvim '..install_path)
 end
-vim.cmd [[packadd packer.nvim]]
+cmd [[packadd packer.nvim]]
 -- Auto compile when there are changes in plugins.lua
-vim.cmd 'autocmd BufWritePost plugins.lua PackerCompile'
+cmd 'autocmd BufWritePost plugins.lua PackerCompile'
 
 -- Install plugins
 require('plugins')

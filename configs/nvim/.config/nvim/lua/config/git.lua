8bd4c538189954897f4b75108e71b9c786f95aea
Aristides Gonzalez <ariguille.gp@gmail.com>
2021-12-20 22:57:14 -0500

    Added git signs on left column

---
+++ configs/nvim/.config/nvim/lua/config/git.lua
@@ -0,0 +1,7 @@
+local utils = require('config.utils')
+utils.map('n', '<Leader>gs', '<cmd>G<CR>')
+utils.map('n', '<Leader>ga', '<cmd>Git fetch --all<CR>')
+utils.map('n', '<Leader>grum', '<cmd>Git rebase upstream/master<CR>')
+utils.map('n', '<Leader>grom', '<cmd>Git rebase origin/master<CR>')
+utils.map('n', '<Leader>gf', '<cmd>diffget //2<CR>')
+utils.map('n', '<Leader>gj', '<cmd>diffget //3<CR>')

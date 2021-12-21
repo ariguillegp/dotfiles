d536a83de8fdaffbde74acca2099447f7cb0a3ce
Aristides Gonzalez <ariguille.gp@gmail.com>
2021-12-20 23:56:50 -0500

    moved telescope to lua

--- configs/nvim/.config/nvim/lua/config/telescope.lua
+++ configs/nvim/.config/nvim/lua/config/telescope.lua
@@ -1,6 +1,14 @@
 -- You dont need to set any of these options. These are the default ones. Only
 -- the loading is important
 require('telescope').setup {
+  defaults = {
+    mappings = {
+      i = {
+        ['<C-u>'] = false,
+        ['<C-d>'] = false,
+      },
+    },
+  },
   extensions = {
     fzf = {
       fuzzy = true,                    -- false will only do exact matching
@@ -20,13 +28,18 @@ require("telescope").load_extension("git_worktree")
 
 local utils = require('config.utils')
 
--- Find files using Telescope command-line sugar.
-utils.map('n', '<Leader>ff', '<cmd>Telescope find_files<CR>')
-utils.map('n', '<Leader>fg', '<cmd>Telescope live_grep<CR>')
-utils.map('n', '<Leader>fb', '<cmd>Telescope buffers<CR>')
-utils.map('n', '<Leader>fh', '<cmd>Telescope help_tags<CR>')
-
--- Fuzzy find git resources
-utils.map('n', '<Leader>gf', '<cmd>Telescope git_files<CR>')
-utils.map('n', '<Leader>gc', '<cmd>Telescope git_commits<CR>')
-utils.map('n', '<Leader>gb', '<cmd>Telescope git_branches<CR>')
+--Add leader shortcuts
+vim.api.nvim_set_keymap('n', '<leader><space>', [[<cmd>lua require('telescope.builtin').buffers()<CR>]], { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>ff', [[<cmd>lua require('telescope.builtin').find_files()<CR>]], { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>df', [[<cmd>lua require('telescope.builtin').find_files({prompt_title = "<Dotfiles>", cwd = "$HOME/.dotfiles"})<CR>]], { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>fb', [[<cmd>lua require('telescope.builtin').current_buffer_fuzzy_find()<CR>]], { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>fh', [[<cmd>lua require('telescope.builtin').help_tags()<CR>]], { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>ft', [[<cmd>lua require('telescope.builtin').tags()<CR>]], { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>fg', [[<cmd>lua require('telescope.builtin').live_grep()<CR>]], { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>so', [[<cmd>lua require('telescope.builtin').tags{ only_current_buffer = true }<CR>]], { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>?', [[<cmd>lua require('telescope.builtin').oldfiles()<CR>]], { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>wl', [[<cmd>lua require('telescope').extensions.git_worktree.git_worktrees()<CR>]], { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>wn', [[<cmd>lua require('telescope').extensions.git_worktree.create_git_worktree()<CR>]], { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>gf', [[<cmd>lua require('telescope.builtin').git_files()<CR>]], { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>gc', [[<cmd>lua require('telescope.builtin').git_commits()<CR>]], { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>gb', [[<cmd>lua require('telescope.builtin').git_branches()<CR>]], { noremap = true, silent = true })

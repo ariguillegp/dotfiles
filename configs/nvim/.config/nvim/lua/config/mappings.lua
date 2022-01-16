50a5126b22335e6d4bd922d892423b57e9ef8ee3
Aristides Gonzalez <ariguille.gp@gmail.com>
2022-01-15 23:17:45 -0500

    silent remaps by default

--- configs/nvim/.config/nvim/lua/config/mappings.lua
+++ configs/nvim/.config/nvim/lua/config/mappings.lua
@@ -16,8 +16,8 @@ utils.map('i', '!', '!<c-g>u')
 utils.map('i', '?', '?<c-g>u')
 
 --- window navigation with vim keys
-utils.map('n', '<Leader>h', '<cmd>wincmd h<CR>')
-utils.map('n', '<Leader>j', '<cmd>wincmd j<CR>')
-utils.map('n', '<Leader>k', '<cmd>wincmd k<CR>')
-utils.map('n', '<Leader>l', '<cmd>wincmd l<CR>')
-utils.map('n', '<Leader>pv', '<cmd>wincmd v<bar> :Ex <bar> :vertical resize 30<CR>')
+utils.map('n', '<leader>h', '<cmd>wincmd h<CR>')
+utils.map('n', '<leader>j', '<cmd>wincmd j<CR>')
+utils.map('n', '<leader>k', '<cmd>wincmd k<CR>')
+utils.map('n', '<leader>l', '<cmd>wincmd l<CR>')
+utils.map('n', '<leader>pv', '<cmd>wincmd v<bar> :Ex <bar> :vertical resize 30<CR>')
--- configs/nvim/.config/nvim/lua/config/utils.lua
+++ configs/nvim/.config/nvim/lua/config/utils.lua
@@ -11,7 +11,7 @@ function utils.opt(scope, key, value)
 end
 
 function utils.map(mode, lhs, rhs, opts)
-  local options = {noremap = true}
+  local options = {noremap = true, silent = true}
   if opts then options = vim.tbl_extend('force', options, opts) end
   vim.api.nvim_set_keymap(mode, lhs, rhs, options)
 end

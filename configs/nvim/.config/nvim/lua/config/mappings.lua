fda78b4dc49529eeae040e6a081b60545461f58f
Aristides Gonzalez <ariguille.gp@gmail.com>
2022-03-29 22:06:32 -0400

    Added mappings for qfixlist navigation

--- configs/nvim/.config/nvim/lua/config/mappings.lua
+++ configs/nvim/.config/nvim/lua/config/mappings.lua
@@ -21,3 +21,8 @@ utils.map('n', '<leader>j', '<cmd>wincmd j<CR>')
 utils.map('n', '<leader>k', '<cmd>wincmd k<CR>')
 utils.map('n', '<leader>l', '<cmd>wincmd l<CR>')
 utils.map('n', '<leader>pv', '<cmd>wincmd v<bar> :Ex <bar> :vertical resize 30<CR>')
+
+--- qfixlist navigation
+utils.map('n', '<C-j>', '<cmd>:cnext<CR>zz')
+utils.map('n', '<C-k>', '<cmd>:cprev<CR>zz')
+utils.map('n', '<leader>a', '<cmd>:cclose<CR>zz')

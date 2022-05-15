59c122c938761ea2c06d8956cb58cb81ef92a80d
Aristides Gonzalez <ariguille.gp@gmail.com>
2022-05-15 12:52:11 -0400

    fixed deprecated fugitive head call

--- configs/nvim/.config/nvim/lua/config/lightline.lua
+++ configs/nvim/.config/nvim/lua/config/lightline.lua
@@ -2,5 +2,5 @@
 vim.g.lightline = {
   colorscheme = 'seoul256',
   active = { left = { { 'mode', 'paste' }, { 'gitbranch', 'readonly', 'filename', 'modified' } } },
-  component_function = { gitbranch = 'fugitive#head' },
+  component_function = { gitbranch = 'FugitiveHead' },
 }

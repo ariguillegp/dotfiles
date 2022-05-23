cd1222790a258597916f83ff05a9b691c867a705
Aristides Gonzalez <ariguille.gp@gmail.com>
2022-05-22 21:34:47 -0400

    Allow installation of pkgs with unfree licenses

--- system/configuration.nix
+++ system/configuration.nix
@@ -125,6 +125,8 @@
     git
   ];
 
+  nixpkgs.config.allowUnfree = true;
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;

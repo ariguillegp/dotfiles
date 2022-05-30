7447a260feacac6f158c4ec41f3262acd6e158d1
Aristides Gonzalez <ariguille.gp@gmail.com>
2022-05-29 21:22:16 -0400

    explicit locale settings

--- system/configuration.nix
+++ system/configuration.nix
@@ -48,11 +48,11 @@ in
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Select internationalisation properties.
-  # i18n.defaultLocale = "en_US.UTF-8";
-  # console = {
-  #   font = "Lat2-Terminus16";
-  #   keyMap = "us";
-  # };
+  i18n.defaultLocale = "en_US.UTF-8";
+  console = {
+    font = "Lat2-Terminus16";
+    keyMap = "us";
+  };
 
   # Enable the X11 windowing system.
   services.xserver = {

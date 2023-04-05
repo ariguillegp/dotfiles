bbb3ca07f64ea0e48c2f119a6291cfb58c2d6158
Aristides Gonzalez <ariguille.gp@gmail.com>
2023-04-04 23:02:25 -0400

    deps: Updated flakes and added comments to build steps

--- rebuild.sh
+++ rebuild.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
 # Update deps
+printf "\n--- Updating flakes ---\n"
 nix flake update
 
 # Rebuild conifg
+printf "\n--- Rebuilding local configurations ---\n"
 sudo nixos-rebuild switch --flake .#nixdso

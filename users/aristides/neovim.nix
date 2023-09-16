787a54cd7a87d77f065a5566c446334fd67ca6b6
Aristides Gonzalez <aristides@glezpol.com>
2023-09-16 04:07:06 -0400

    removed uneeded nvim block

--- users/aristides/neovim.nix
+++ users/aristides/neovim.nix
@@ -1,12 +1,6 @@
 { pkgs, ... }:
 
 {
-  #nixpkgs.overlays = [
-  #  (import (builtins.fetchTarball {
-  #    url = https://github.com/nix-community/neovim-nightly-overlay/archive/master.tar.gz;
-  #  }))
-  #];
-
   programs = {
     neovim = {
       enable = true;

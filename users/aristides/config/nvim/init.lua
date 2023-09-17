6f2a894ff30144c02d6f1ab5abe8e3c94c93f1ef
Aristides Gonzalez <aristides@glezpol.com>
2023-09-16 23:51:05 -0400

    lsp: use legacy version of fidget plugin for now

--- users/aristides/config/nvim/init.lua
+++ users/aristides/config/nvim/init.lua
@@ -50,7 +50,7 @@ require('lazy').setup({
 
       -- Useful status updates for LSP
       -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
-      { 'j-hui/fidget.nvim', opts = {} },
+      { 'j-hui/fidget.nvim', tag = "legacy", event = "LspAttach", opts = {} },
 
       -- Additional lua configuration, makes nvim stuff amazing!
       'folke/neodev.nvim',

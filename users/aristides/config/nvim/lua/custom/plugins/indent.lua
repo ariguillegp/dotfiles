6001724c28ba8cda130b484a9af0333cb2f665bb
Aristides Gonzalez <aristides@glezpol.com>
2023-12-21 10:20:19 -0500

    feat:moved to indent blankline v3

--- users/aristides/config/nvim/lua/custom/plugins/indent.lua
+++ users/aristides/config/nvim/lua/custom/plugins/indent.lua
@@ -1,12 +1,12 @@
 return {
   { -- Add indentation guides even on blank lines
-    'lukas-reineke/indent-blankline.nvim',
-    -- Enable `lukas-reineke/indent-blankline.nvim`
-    -- See `:help indent_blankline.txt`
-    opts = {
-      char = '┊',
-      show_trailing_blankline_indent = false,
-      show_end_of_line = true,
-    },
+    -- See `:help ibl`
+    "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
+    config = function()
+      require("ibl").setup {
+        indent = { char = "┊" },
+      }
+    end,
   },
 }

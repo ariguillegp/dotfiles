55279fb5c3773675748798b0b8a95543c61151d0
Aristides Gonzalez <aristides@glezpol.com>
2023-12-29 15:09:24 -0500

    fmt: removed unused code

--- modules/home-manager/config/nvim/plugins/lsp.lua
+++ modules/home-manager/config/nvim/plugins/lsp.lua
@@ -38,24 +38,6 @@ local on_attach = function(_, bufnr)
   end, '[W]orkspace [L]ist Folders')
 end
 
--- -- document existing key chains
--- require('which-key').register {
---   ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
---   ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
---   ['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
---   ['<leader>h'] = { name = 'Git [H]unk', _ = 'which_key_ignore' },
---   ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
---   ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
---   ['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
---   ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
--- }
--- -- register which-key VISUAL mode
--- -- required for visual <leader>hs (hunk stage) to work
--- require('which-key').register({
---   ['<leader>'] = { name = 'VISUAL <leader>' },
---   ['<leader>h'] = { 'Git [H]unk' },
--- }, { mode = 'v' })
-
 -- Setup neovim lua configuration
 require('neodev').setup()
 
@@ -68,6 +50,7 @@ local lspconfig = require('lspconfig')
 -- Enable the following language servers
 local servers = { "bashls", "dockerls", "rnix", "ruff_lsp", "rust_analyzer" }
 
+-- Basic LSP setup
 for _, server_name in ipairs(servers) do
   lspconfig[server_name].setup {
     on_attach = on_attach,
@@ -80,6 +63,7 @@ local runtime_path = vim.split(package.path, ';')
 table.insert(runtime_path, 'lua/?.lua')
 table.insert(runtime_path, 'lua/?/init.lua')
 
+-- Non-standard LSP setup
 lspconfig.lua_ls.setup {
   on_attach = on_attach,
   capabilities = capabilities,

f7efe74be8783b7690836c2c04dc3fa99d170b77
Aristides Gonzalez <aristides@crescentcyber.com>
2024-06-09 16:28:39 -0400

    .

--- rebuild.sh
+++ rebuild.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 pushd ~/.dotfiles
 printf "\nRebuilding user configurations\n\n"
-home-manager switch  --experimental-features 'nix-command flakes' --impure --flake '.#'
+home-manager switch  --experimental-features 'nix-command flakes' --impure --flake '.#aristides'
 printf "Rebuilding system configurations\n\n"
 sudo nixos-rebuild switch --flake '.#'
 popd
